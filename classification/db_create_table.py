5624454bbdc7c7a74c70d51d92d09678d582d7f3
Sophie Buzaeva <sofibuz@mail.ru>
2024-03-20 10:58:50 +0300

    New version of db

--- classification/db_create_table.py
+++ classification/db_create_table.py
@@ -6,13 +6,28 @@ def create_tables():
     try:
         with local_conn() as con:
             cursor = con.cursor()
-            cursor.execute("CREATE SEQUENCE your_seq;")
+            cursor.execute("CREATE SEQUENCE seo_seq;")
             query = """CREATE TABLE \"images\" (
-                       \"id\" INTEGER PRIMARY KEY default nextval('your_seq'),
+                       \"id\" INTEGER PRIMARY KEY default nextval('seo_seq'),
                        \"category\" TEXT,
                        \"type\" TEXT,
                        \"name\" TEXT,
                        CONSTRAINT image_unique UNIQUE (category, type, name))"""
             cursor.execute(query)
+        print("Table images and seo_seq CREATED!")
     except:
         print("Table images are already exist")
+
+    try:
+        with local_conn() as con:
+            cursor = con.cursor()
+            cursor.execute("CREATE SEQUENCE celery_seq;")
+            query = """CREATE TABLE \"celery_training_status\" (
+                       \"id\" INTEGER PRIMARY KEY,
+                       \"task_id\" VARCHAR(155),
+                       \"status\" VARCHAR(50),
+                       CONSTRAINT task_id_unique UNIQUE (task_id))"""
+            cursor.execute(query)
+        print("Table celery_training_status and celery_seq CREATED!")
+    except:
+        print("Table celery_training_status are already exist")
--- classification/db_fill.py
+++ classification/db_fill.py
@@ -21,11 +21,10 @@ def fill_db(paths):
     with local_conn() as con:
         cursor = con.cursor()
         for i in range(len(paths)):
-            cursor.execute("INSERT INTO images (category, type, name)\
-                            VALUES (?, ?, ?)", (paths[i].parts[-3:-2][0],
-                                                   paths[i].parts[-2:-1][0],
-                                                   paths[i].parts[-1:][0],
-                                                  ))
+            query = f""" INSERT INTO images (category, type, name)
+                         VALUES ('{paths[i].parts[-3:-2][0]}', '{paths[i].parts[-2:-1][0]}', '{paths[i].parts[-1:][0]}')
+                     """
+            cursor.execute(query)
 
 def main():
     create_tables()
